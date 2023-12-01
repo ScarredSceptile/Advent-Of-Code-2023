c8efccda393dbb7800b4c43f637097845c3292b4
Jonas Hægdahl <jonas.haegd@gmail.com>
2023-12-01 14:47:37 +0100

    Complete Day1

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -4,12 +4,105 @@
     {
         public void Star1()
         {
-            throw new NotImplementedException();
+            var input = Input.Get("Day1");
+            int value = 0;
+            foreach (var item in input)
+            {
+                string num = "";
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (char.IsDigit(item[i]))
+                    {
+                        num += item[i];
+                        break;
+                    }
+                }
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    if (char.IsDigit(item[i]))
+                    {
+                        num += item[i];
+                        break;
+                    }
+                }
+
+                value += int.Parse(num);
+            }
+            Console.WriteLine(value);
         }
+
+        private Dictionary<string, string> Digits = new Dictionary<string, string>
+        {
+            {"one", "1" },
+            {"two", "2" },
+            {"three", "3" },
+            {"four", "4" },
+            {"five", "5" },
+            {"six", "6" },
+            {"seven", "7" },
+            {"eight", "8" },
+            {"nine", "9" },
+        };
 
         public void Star2()
         {
-            throw new NotImplementedException();
+            var input = Input.Get("Day1");
+            int value = 0;
+            foreach (var item in input)
+            {
+                string num = "";
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (char.IsDigit(item[i]))
+                    {
+                        num += item[i];
+                        break;
+                    }
+                    var start = GetStart(item[i..]);
+                    if (start != "")
+                    {
+                        num += start;
+                        break;
+                    }
+                }
+                for (int i = item.Length - 1; i >= 0; i--)
+                {
+                    if (char.IsDigit(item[i]))
+                    {
+                        num += item[i];
+                        break;
+                    }
+                    var start = GetEnd(item[..(i + 1)]);
+                    if (start != "")
+                    {
+                        num += start;
+                        break;
+                    }
+                }
+
+                value += int.Parse(num);
+            }
+            Console.WriteLine(value);
+        }
+
+        private string GetStart(string input)
+        {
+            foreach (var item in Digits)
+            {
+                if (input.StartsWith(item.Key))
+                    return item.Value;
+            }
+            return "";
+        }
+
+        private string GetEnd(string input)
+        {
+            foreach (var item in Digits)
+            {
+                if (input.EndsWith(item.Key))
+                    return item.Value;
+            }
+            return "";
         }
     }
 }
