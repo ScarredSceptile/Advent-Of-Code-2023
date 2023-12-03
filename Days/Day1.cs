45c1d1905195c1dfe791e89204d19d76c6d628df
jonas <jonas.haegd@gmail.com>
2023-12-03 18:55:12 +0100

    Refactor Day 1

--- Days/Day1.cs
+++ Days/Day1.cs
@@ -8,24 +8,9 @@
             int value = 0;
             foreach (var item in input)
             {
-                string num = "";
-                for (int i = 0; i < item.Length; i++)
-                {
-                    if (char.IsDigit(item[i]))
-                    {
-                        num += item[i];
-                        break;
-                    }
-                }
-                for (int i = item.Length - 1; i >= 0; i--)
-                {
-                    if (char.IsDigit(item[i]))
-                    {
-                        num += item[i];
-                        break;
-                    }
-                }
-
+                string num = $"{item.FirstOrDefault(char.IsDigit)}{item.LastOrDefault(char.IsDigit)}";
+                if (num == "\0\0")
+                    continue;
                 value += int.Parse(num);
             }
             Console.WriteLine(value);
@@ -33,15 +18,15 @@
 
         private Dictionary<string, string> Digits = new Dictionary<string, string>
         {
-            {"one", "1" },
-            {"two", "2" },
-            {"three", "3" },
-            {"four", "4" },
-            {"five", "5" },
-            {"six", "6" },
-            {"seven", "7" },
-            {"eight", "8" },
-            {"nine", "9" },
+            {"one", "o1e" },
+            {"two", "t2o" },
+            {"three", "t3e" },
+            {"four", "f4r" },
+            {"five", "f5e" },
+            {"six", "s6x" },
+            {"seven", "s7n" },
+            {"eight", "e8t" },
+            {"nine", "n9e" },
         };
 
         public void Star2()
@@ -50,59 +35,17 @@
             int value = 0;
             foreach (var item in input)
             {
-                string num = "";
-                for (int i = 0; i < item.Length; i++)
-                {
-                    if (char.IsDigit(item[i]))
-                    {
-                        num += item[i];
-                        break;
-                    }
-                    var start = GetStart(item[i..]);
-                    if (start != "")
-                    {
-                        num += start;
-                        break;
-                    }
-                }
-                for (int i = item.Length - 1; i >= 0; i--)
-                {
-                    if (char.IsDigit(item[i]))
-                    {
-                        num += item[i];
-                        break;
-                    }
-                    var start = GetEnd(item[..(i + 1)]);
-                    if (start != "")
-                    {
-                        num += start;
-                        break;
-                    }
-                }
+                string formatted = item;
+                foreach(var digit in Digits)
+                    formatted = formatted.Replace(digit.Key, digit.Value);
+
+                string num = $"{formatted.FirstOrDefault(char.IsDigit)}{formatted.LastOrDefault(char.IsDigit)}";
+                if (num == "\0\0")
+                    continue;
 
                 value += int.Parse(num);
             }
             Console.WriteLine(value);
-        }
-
-        private string GetStart(string input)
-        {
-            foreach (var item in Digits)
-            {
-                if (input.StartsWith(item.Key))
-                    return item.Value;
-            }
-            return "";
-        }
-
-        private string GetEnd(string input)
-        {
-            foreach (var item in Digits)
-            {
-                if (input.EndsWith(item.Key))
-                    return item.Value;
-            }
-            return "";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Advent_Of_Code_2023
     {
         static void Main(string[] args)
         {
-            Day day = new Day3();
+            Day day = new Day1();
             day.Star2();
         }
     }
