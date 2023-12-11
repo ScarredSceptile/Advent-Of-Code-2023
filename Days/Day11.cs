a8a9bc0fcfc76f6e66b064855201c2b8c5ec8647
jonas <jonas.haegd@gmail.com>
2023-12-11 21:07:10 +0100

    Simple Day11 Refactor

--- Days/Day11.cs
+++ Days/Day11.cs
@@ -11,28 +11,23 @@ namespace Advent_Of_Code_2023.Days
     {
         public void Star1()
         {
-            var galaxies = GetGalaxies();
-
-            for (int i = galaxies.Min(n => n.X) + 1; i < galaxies.Max(n => n.X); i++)
-                if (galaxies.Any(x => x.X == i) == false)
-                    galaxies.Where(x => x.X < i).ToList().ForEach(x => x.X--);
-
-            for (int i = galaxies.Min(n => n.Y) + 1; i < galaxies.Max(n => n.Y); i++)
-                if (galaxies.Any(x => x.Y == i) == false)
-                    galaxies.Where(x => x.Y < i).ToList().ForEach(x => x.Y--);
-
-            int total = 0;
-            int c = 0;
-            for (int i = 0; i < galaxies.Count - 1; i++)
-                for (int j = i+1; j < galaxies.Count; j++)
-                    total += Math.Abs(galaxies[j].X - galaxies[i].X) + Math.Abs(galaxies[j].Y - galaxies[i].Y);
-
-            Console.WriteLine(total);
+            Console.WriteLine(GetGalaxyDistance(2));
         }
 
         public void Star2()
         {
-            var galaxies = GetGalaxies();
+            Console.WriteLine(GetGalaxyDistance(1000000));
+        }
+
+        private long GetGalaxyDistance(long extension)
+        {
+            var input = Input.Get("Day11");
+            List<Galaxy> galaxies = new();
+            for (int i = 0; i  < input.Length; i++)
+                for (int j = 0; j < input[i].Length; j++)
+                    if (input[i][j] == '#')
+                        galaxies.Add(new Galaxy(j, i));
+
             List<int> XSpread = new();
             List<int> YSpread = new();
 
@@ -49,7 +44,7 @@ namespace Advent_Of_Code_2023.Days
             for (int i = 0; i < galaxies.Count - 1; i++)
                 for (int j = i+1; j < galaxies.Count; j++)
                 {
-                    long spread = 1000000;
+                    long spread = extension;
                     long xDist = Math.Abs(galaxies[j].X - galaxies[i].X);
                     int[] X = new[] { galaxies[j].X, galaxies[i].X };
                     xDist += XSpread.Where(x => x > X.Min() && x < X.Max()).Count() * (spread - 1);
@@ -60,26 +55,13 @@ namespace Advent_Of_Code_2023.Days
 
                     total += xDist + yDist;
                 }
-
-            Console.WriteLine(total);
+            return total;
         }
-
         private class Galaxy
         {
             public int X;
             public int Y;
             public Galaxy(int x, int y) { X = x; Y = y; }
-        }
-
-        private List<Galaxy> GetGalaxies()
-        {
-            var input = Input.Get("Day11");
-            List<Galaxy> galaxies = new();
-            for (int i = 0; i  < input.Length; i++)
-                for (int j = 0; j < input[i].Length; j++)
-                    if (input[i][j] == '#')
-                        galaxies.Add(new Galaxy(j, i));
-            return galaxies;
         }
     }
 }
