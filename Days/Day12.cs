4088ae9c6a527cbdf396fc42ee243c1456cf7f9b
jonas <jonas.haegd@gmail.com>
2023-12-13 20:59:50 +0100

    Complete Day 13

---
+++ Days/Day12.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2023.Days
+{
+    internal class Day12 : Day
+    {
+        public void Star1()
+        {
+            var input = Input.Get("Day12");
+            int total = 0;
+            foreach (var item in input)
+            {
+                var springs = item.Split(" ")[0].ToArray();
+                var brokenCounts = item.Split(" ")[1].Split(",").Select(int.Parse).ToArray();
+
+                var count = Math.Pow(2, springs.Count(n => n == '?'));
+                var len = Convert.ToString((int)count, 2).Length - 1;
+                Console.WriteLine(count);
+                for (int i = 0; i < count; i++)
+                {
+                    string rpl = Convert.ToString(i, 2);
+                    while (rpl.Length < len)
+                        rpl = '0' + rpl;
+                    var idx = (char[])springs.Clone();
+                    for (int j = 0; j < rpl.Length; j++)
+                        for (int k = 0; k < idx.Length; k++)
+                            if (idx[k] == '?')
+                            {
+                                idx[k] = rpl[j] == '0' ? '.' : '#';
+                                break;
+                            }
+                    var broken = new string(idx).Split('.').Where(n => n.Length > 0).Select(n => n.Length).ToArray();
+
+                    if (broken.Length == brokenCounts.Length)
+                    {
+                        bool equal = true;
+                        for (int j = 0; j < broken.Length; j++)
+                            if (broken[j] != brokenCounts[j])
+                            {
+                                equal = false;
+                                break;
+                            }
+                        if (equal)
+                            total++;
+                    }
+                }
+            }
+            Console.WriteLine(total);
+        }
+
+        public void Star2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Days/Day13.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code_2023.Days
+{
+    internal class Day13 : Day
+    {
+        public void Star1()
+        {
+            var input = Input.GetSingle("Day13").Split("\r\n\r\n");
+            int vertical = 0;
+            int horizontal = 0;
+
+            foreach (var field in input.Select(n => n.Split("\r\n")))
+            {
+                vertical += GetVertical(field);
+                horizontal += GetHorizontal(field);
+            }
+            Console.WriteLine(vertical + horizontal * 100);
+        }
+
+        public void Star2()
+        {
+            var input = Input.GetSingle("Day13").Split("\r\n\r\n");
+            int vertical = 0;
+            int horizontal = 0;
+
+            foreach (var field in input.Select(n => n.Split("\r\n")))
+            {
+                var notVert = GetVertical(field);
+                var notHori = GetHorizontal(field);
+                vertical += GetNewVertical(field, notVert);
+                horizontal += GetNewHorizontal(field, notHori);
+            }
+            Console.WriteLine(vertical + horizontal * 100);
+        }
+
+        private int GetHorizontal(string[] field, int avoid = -1)
+        {
+            for (int i = 0; i < field.Length - 1; i++)
+            {
+                bool isMirror = true;
+                for (int j = 1; j <= i + 1; j++)
+                {
+                    if (i - (j-1) == -1 || i+j == field.Length)
+                        break;
+                    for (int k = 0; k < field[0].Length; k++)
+                        if (field[i - (j - 1)][k] != field[i+j][k])
+                        {
+                            isMirror = false; break;
+                        }
+                }
+                if (isMirror && i != avoid - 1)
+                    return i+1;
+            }
+            return 0;
+        }
+
+        private int GetNewHorizontal(string[] field, int avoid)
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                var changed = (string[])field.Clone();
+                for (int j = 0; j < field[0].Length; j++)
+                {
+                    changed[i] = Replace(field[i], j);
+                    var value = GetHorizontal(changed, avoid);
+                    if (value != 0)
+                        return value;
+                }
+            }
+            return 0;
+        }
+
+        private int GetVertical(string[] field, int avoid = -1)
+        {
+            for (int i = 0; i < field[0].Length - 1; i++)
+            {
+                bool isMirror = true;
+                for (int j = 1; j <= i + 1; j++)
+                {
+                    if (i - (j-1) == -1 || i+j == field[0].Length)
+                        break;
+                    for (int k = 0; k < field.Length; k++)
+                        if (field[k][i - (j - 1)] != field[k][i + j])
+                        {
+                            isMirror = false; break;
+                        }
+                }
+                if (isMirror && i != avoid - 1)
+                    return i+1;
+            }
+            return 0;
+        }
+
+        private int GetNewVertical(string[] field, int avoid = -1)
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                var changed = (string[])field.Clone();
+                for (int j = 0; j < field[0].Length; j++)
+                {
+                    changed[i] = Replace(field[i], j);
+                    var value = GetVertical(changed, avoid);
+                    if (value != 0)
+                        return value;
+                }
+            }
+            return 0;
+        }
+
+        private string Replace(string inp, int idx)
+        {
+            StringBuilder sb = new(inp);
+            sb[idx] = sb[idx] == '.' ? '#' : '.';
+            return sb.ToString();
+        }
+    }
+}
